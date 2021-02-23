941fa5033e044780bc16cb7c449fc9a57449f0c9
Ewa Trzemżalska <trzemson@gmail.com>
2021-02-23 16:23:28 +0100

    Add all untracked and modified files

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import jwt
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
-
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
@@ -79,7 +78,7 @@ def auth():
 
     user_data = body
 
-    return jsonify(token=_get_jwt(user_data).decode('utf-8'))
+    return jsonify(token=_get_jwt(user_data))
 
 
 @APP.route('/contents', methods=['GET'])
